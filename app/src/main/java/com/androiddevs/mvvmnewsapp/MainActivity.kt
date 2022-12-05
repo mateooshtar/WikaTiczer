84d41e1efb778343c5bd5768910da78f28e691e2
prodige <mateoosh_tar@o2.pl>
2022-12-05 13:08:07 +0100

    commit w trakcie WHEN

--- app/src/main/java/com/androiddevs/mvvmnewsapp/MainActivity.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.androiddevs.mvvmnewsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,8 +10,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var z = 0
+//
+//
+//        val toast = if (z > 0 ) { Toast.makeText(this, "Dodatnia", Toast.LENGTH_LONG).show(); println("Dodatnia liczba"); "Dodatniaaaaa"} else Toast.makeText(this, "Ujemna", Toast.LENGTH_SHORT).show()
+//        println(toast)
 
 
+//        val kotlin = when {
+//            z == "musztarda" -> "Muśtryk"
+//            z == "keczup" -> "kepuć"
+//            z == "Programming language" -> "ucz sie uju"
+//            else -> "miejscowość"
+//        }
+
+        when(z) {
+            1 -> "dodatnia"
+            -1 -> "ujemna"
+            else -> "urojona"
+        }
+
+
+        println(kotlin)
+
+        Toast.makeText(this, kotlin, Toast.LENGTH_LONG).show()
 
     }
 }
