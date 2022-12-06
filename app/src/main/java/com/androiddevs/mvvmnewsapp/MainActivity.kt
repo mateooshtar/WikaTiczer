ec6ee4b0b9bf4c34fb18c63d584e2e26e5bb6944
prodige <mateoosh_tar@o2.pl>
2022-12-06 11:46:17 +0100

    przed obiektówką, po lambda i f wyzszego rzędu

--- app/src/main/java/com/androiddevs/mvvmnewsapp/MainActivity.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/MainActivity.kt
@@ -10,8 +10,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var stefanMatematyk = lambda(10,50) {
+            x,y -> (x+y).toString()
+        }
+
+        println(stefanMatematyk)
+
+        var table = listOf(1,2,3,4,5)
+
+        table.forEach{item ->
+            var i = item * item
+            for (x in 1..10) {
+                println("$i huehueeeee")
+            }
+        }
 
 
 
     }
+
+
+    fun lambda(a: Int, b: Int, c: (Int,Int) -> String) : String {
+        return ("$a + $b = ${a+b}. ${c(a, b)}")
+    }
+
+    var add = { a: Int, b: Int -> a - b }
+
+
+
 }
