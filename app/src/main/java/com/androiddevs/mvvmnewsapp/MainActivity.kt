65cd47fc224457d23134d39709634d1de3920ce0
prodige <mateoosh_tar@o2.pl>
2022-12-06 08:57:10 +0100

    Przed lambdą - powtórka

--- app/src/main/java/com/androiddevs/mvvmnewsapp/MainActivity.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/MainActivity.kt
@@ -10,15 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var arr = arrayOf<Int>(1,2,3,4,5)
-
-        for ((i, v) in arr.withIndex()) {
-            println("$i: $v")
-            Toast.makeText(this,"$i: $v",Toast.LENGTH_LONG).show()
-        }
-
-        for (x in 10 downTo 1) println()
-
 
 
 
