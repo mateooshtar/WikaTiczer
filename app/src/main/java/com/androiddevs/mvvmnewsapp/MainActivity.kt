50c1204cc2c927268648e8701f7b17126bc34b0d
prodige <mateoosh_tar@o2.pl>
2022-12-07 13:23:43 +0100

    Zabawa dziedziczeniem

--- app/src/main/java/com/androiddevs/mvvmnewsapp/MainActivity.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/MainActivity.kt
@@ -11,31 +11,52 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var stefek = Person("Stefan", "Brzęczyszczykiewicz", 502175684)
-        stefek.whoAreYou()
-    }
-}
 
-
-abstract class Person {
-
-    abstract var name: String
-    abstract var surname: String
-    abstract var phone: Long
-    abstract var gender: String
+        var person1 = Person("Joanna","Female",555555555, listOf("Pranie", "Sprzątanie", "Gotowanie"))
+        person1.showMeYourSkills()
+        person1.whatIsYourName2()
+        person1.whatIsYourName()
 
 
 
-    fun whoAreYou() {
-        return println("Siema, jestem $name $surname. Mój nr telefonu to $phone")
     }
 
-    abstract fun setGender(genderToSet: String) : String
-    abstract fun getGender() : Int
-    abstract fun goPee() : String
+    abstract class Human {
+        constructor(name: String, gender: String, phone: Long, skills: List<String>) {
+            this.name = name
+            this.phone = phone
+            this.gender = gender
+            this.skills = skills
+        }
 
-}
+        var name: String
+        var gender: String
+        var phone: Long
+        abstract var skills: List<String>
 
-class Man(name: String, surname:String, gender: String) {
+        fun whatIsYourName() : Unit {
+            return println("My name is $name")
+        }
+
+        var whatIsYourName2 = { println("lambda My name is $this.name")} //tu nie wiem jak się odwołać do name :/
+
+        abstract fun showMeYourSkills(): Unit
+    }
+
+
+    class Person(name: String, gender: String, phone: Long, listOfSkills: List<String> ) : Human(name, gender, phone, listOfSkills) {
+        override var skills: List<String> = listOfSkills
+
+        override fun showMeYourSkills(): Unit {
+            var stringSkills= "Moje umiejętności to:" //nie powinienem tutaj tego pisać, tylko inaczej zaimplemetować, ale walić to w tym przykładzie xd
+
+            for (s in skills) {
+                stringSkills = "$stringSkills $s"
+            }
+            return println(stringSkills)
+        }
+
+    }
+
 
 }
